42110e163f00517d858cdd1e89afb437534ea2bb
chenjb58 <510549570@qq.com>
2019-09-22 13:27:53 +0800

    upload from macOS

---
+++ 第二次作业/paowuxian&soloasys/Assets/Scripts/paowuxian.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//using Transform
+public class paowuxian : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // initial speed
+    public float initSpeed = 10f;
+    // two direction
+    private float xSpeed, ySpeed;
+    // initial angle 
+    public float angle = Mathf.PI / 3;
+    // gravity s
+    private float gravity = 9.8f;
+
+    void Start()
+    {
+        // phisical formula
+        xSpeed = initSpeed * Mathf.Cos(angle);
+        ySpeed = initSpeed * Mathf.Sin(angle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //motion vector per deltaTime
+        Vector3 vec = new Vector3(Time.deltaTime * xSpeed, Time.deltaTime * ySpeed, 0);
+        //change positon
+        this.transform.position += vec;
+        //ySpeed is affected by gravityss
+        ySpeed -= gravity * Time.deltaTime;
+    }
+}
